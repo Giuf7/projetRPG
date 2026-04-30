92695a4072e36111c4052b6dd10c1a1245bba5b4
lhubeaux <louis.hubeaux@gmail.com>
2026-04-30 16:01:20 +0200

    liaison avec personnage

--- combat.py
+++ combat.py
@@ -1,4 +1,4 @@
-import all from personage.py
+from personnage import hp, atk, nom
 import random
 
 #Ajout des stats des monstres
@@ -12,9 +12,9 @@ nomMonstre2 = "Bandit"
 
 #Création de la fonction
 def combattreMonstre1():
-    hp = joueurhp
-    atk = joueuratk
-    nom = joueurnom
+    joueurhp = hp
+    joueuratk = atk
+    joueurnom = nom
     monstreHP = hpMonstre1
     monstreATK = atkMonstre1
     monstreNom = nomMonstre1
@@ -27,28 +27,28 @@ def combattreMonstre1():
             tour = False
 
 
-    while hp > 0 and monstreHP > 0:
-        print(f"{nom} est attaqué par {monstreNom}!!")
+    while joueurhp > 0 and monstreHP > 0:
+        print(f"{joueurnom} est attaqué par {monstreNom}!!")
         while tour:
-            print(f"{nom} attaque {monstreNom} pour {atk} dégâts.")
-            monstreHP = monstreHP - atk
+            print(f"{joueurnom} attaque {monstreNom} pour {joueuratk} dégâts.")
+            monstreHP = monstreHP - joueuratk
             print(f"Le monstre a {monstreHP} points de vie restants, il se prépare à attaquer...")
             tour = not tour
         else:
-             print(f"Le monstre attaque {nom} pour {monstreATK} dégâts.")
-             hp = hp - monstreATK
-             print(f"{nom} a {hp} points de vie restants. À votre tour!")
+             print(f"Le monstre attaque {joueurnom} pour {monstreATK} dégâts.")
+             joueurhp = joueurhp - monstreATK
+             print(f"{joueurnom} a {joueurhp} points de vie restants. À votre tour!")
              tour = not tour
     else:
         if monstreHP <= 0:
             print("Le monstre est vaincu!")
-        elif hp <= 0:
+        elif joueurhp <= 0:
              print("Le monstre a gagné!")
         
 def combattreMonstre2():
-    hp = joueurhp
-    atk = joueuratk
-    nom = joueurnom
+    joueurhp = hp
+    joueuratk = atk
+    joueurnom = nom
     monstreHP = hpMonstre2
     monstreATK = atkMonstre2
     monstreNom = nomMonstre2
@@ -61,20 +61,22 @@ def combattreMonstre2():
             tour = False
 
 
-    while hp > 0 and monstreHP > 0:
-        print(f"{nom} est attaqué par {monstreNom}!!")
+    while joueurhp > 0 and monstreHP > 0:
+        print(f"{joueurnom} est attaqué par {monstreNom}!!")
         while tour:
-            print(f"{nom} attaque {monstreNom} pour {atk} dégâts.")
-            monstreHP = monstreHP - atk
+            print(f"{joueurnom} attaque {monstreNom} pour {joueuratk} dégâts.")
+            monstreHP = monstreHP - joueuratk
             print(f"Le monstre a {monstreHP} points de vie restants, il se prépare à attaquer...")
             tour = not tour
         else:
-             print(f"Le monstre attaque {nom} pour {monstreATK} dégâts.")
-             hp = hp - monstreATK
-             print(f"{nom} a {hp} points de vie restants. À votre tour!")
+             print(f"Le monstre attaque {joueurnom} pour {monstreATK} dégâts.")
+             joueurhp = joueurhp - monstreATK
+             print(f"{joueurnom} a {joueurhp} points de vie restants. À votre tour!")
              tour = not tour
     else:
         if monstreHP <= 0:
             print("Le monstre est vaincu!")
-        elif hp <= 0:
+        elif joueurhp <= 0:
              print("Le monstre a gagné!")
+
+combattreMonstre1()
