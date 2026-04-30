aa59d2d3f685703a064ff33ffb3a9e76cd9a7600
Giuseppe Formolo <piino-hsp@live.be>
2026-04-30 14:50:27 +0200

    màj personnage.py

--- personnage.py
+++ personnage.py
@@ -1,8 +1,21 @@
-atk = 10
-hp = 100
+# Initialisation des variables
+nom = ""
+atk = 0
+hp = 0
 inventaire = []
-nom = str(input("Quel est le nom de votre personnage ?:"))
+
+def creation_personnage():
+    global nom, atk, hp, inventaire
+    
+    nom = str(input("Quel est le nom de ton personnage ? : "))
+    atk = 10
+    hp = 100
+    inventaire = [""]
+    print(f"\nBienvenue, {nom} ! Ton aventure commence maitenant.")
 
 def afficher_stats():
-    print(f"{nom}: Vous avez {hp} point de vue et {atk} d'attaque.")
-    print(f"Votre inventaire: {inventaire}")
+    print(f"--- Stats de {nom} ---")
+    print(f"Points de vie : {hp}")
+    print(f"Attaque : {atk}")
+    print(f"Inventaire : {inventaire}")
+    print("----------------------")
