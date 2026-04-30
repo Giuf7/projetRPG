46819a3e56da786fc0f55d2b97c1b76742dae246
Logan Parent <l.parent@stag.technofuturtic.education>
2026-04-30 16:37:30 +0200

    Maj "enigmes.py"

--- enigmes.py
+++ enigmes.py
@@ -3,7 +3,7 @@ from personnage import inventaire
 enigme_salle_est = "Je suis toujours devant toi, mais tu ne peux jamais me voir. Qui suis-je ?"
 reponse_salle_est = "le futur"
 enigme_salle_ouest = "Je peux être cassé sans jamais être touché. Qui suis-je ?"
-reponse_salle_est = "le silence"
+reponse_salle_ouest = "le silence"
 objet_salle_est = "potion"
 objet_salle_ouest = "épée"
 
