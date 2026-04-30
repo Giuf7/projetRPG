bfeb9b0af7b4df20a98a04037ca85e499896eba9
lhubeaux <louis.hubeaux@gmail.com>
2026-04-30 15:52:17 +0200

    fonction pour le 2e monstre

--- combat.py
+++ combat.py
@@ -4,12 +4,9 @@ hpMonstre1 = 40
 nomMonstre1 = "Guerrier gobelin"
 
 atkMonstre2 = 5
-atkMonstre2 = 30
+hpMonstre2 = 30
 nomMonstre2 = "Bandit"
 
-joueurhp = 100
-joueuratk = 10
-joueurnom = "louis"
 #Création de la fonction
 import random
 def combattreMonstre1():
@@ -46,4 +43,37 @@ def combattreMonstre1():
         elif hp <= 0:
              print("Le monstre a gagné!")
         
-combattreMonstre1()
+def combattreMonstre2():
+    hp = joueurhp
+    atk = joueuratk
+    nom = joueurnom
+    monstreHP = hpMonstre2
+    monstreATK = atkMonstre2
+    monstreNom = nomMonstre2
+    randomCommence = random.randint (1,2)
+    if randomCommence == 2:
+            tour = True
+            print("Le joueur commence")
+    else:
+            print("Le monstre commence!")
+            tour = False
+
+
+    while hp > 0 and monstreHP > 0:
+        print(f"{nom} est attaqué par {monstreNom}!!")
+        while tour:
+            print(f"{nom} attaque {monstreNom} pour {atk} dégâts.")
+            monstreHP = monstreHP - atk
+            print(f"Le monstre a {monstreHP} points de vie restants, il se prépare à attaquer...")
+            tour = not tour
+        else:
+             print(f"Le monstre attaque {nom} pour {monstreATK} dégâts.")
+             hp = hp - monstreATK
+             print(f"{nom} a {hp} points de vie restants. À votre tour!")
+             tour = not tour
+    else:
+        if monstreHP <= 0:
+            print("Le monstre est vaincu!")
+        elif hp <= 0:
+             print("Le monstre a gagné!")
+combattreMonstre2()
