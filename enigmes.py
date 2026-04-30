6cf0600154b2cb54df8c4273aa7fbff080e22af6
Logan Parent <l.parent@stag.technofuturtic.education>
2026-04-30 15:12:24 +0200

    Déclaration et initialisation des variables

---
+++ enigmes.py
@@ -0,0 +1,7 @@
+enigme_salle_est = "Je suis toujours devant toi, mais tu ne peux jamais me voir. Qui suis-je ?"
+reponse_salle_est = "le futur"
+enigme_salle_ouest = "Je peux être cassé sans jamais être touché. Qui suis-je ?"
+reponse_salle_est = "le silence"
+objet_salle_est = "potion"
+objet_salle_ouest = "épée"
+
