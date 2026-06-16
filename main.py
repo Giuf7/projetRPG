6c2a37803f3ea36430b3051416731262d5445587
Giuseppe Formolo <g.formolo@stag.technofuturtic.education>
2026-06-16 14:28:12 +0200

    Refactor import statements for module names

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from personnage import creation_personnage, afficher_stats, inventaire
-from combat import combattreMonstre1, combattreMonstre2, atkMonstre1, hpMonstre1, nomMonstre1, atkMonstre2, hpMonstre2, nomMonstre2
-from enigmes import prendre_objet_est, prendre_objet_ouest, resoudre_enigme_est, resoudre_enigme_ouest
-from ModuleExplorationSalles import deplacer, afficher_salle
+from module_personnage import creation_personnage, afficher_stats, inventaire
+from module_combat import combattreMonstre1, combattreMonstre2, atkMonstre1, hpMonstre1, nomMonstre1, atkMonstre2, hpMonstre2, nomMonstre2
+from module_enigmes import prendre_objet_est, prendre_objet_ouest, resoudre_enigme_est, resoudre_enigme_ouest
+from module_exploration import deplacer, afficher_salle
 
 
 creation_personnage()
